c8564223c90181f68ec1d1507b99e891d33b0631
RayVilaca <cecilia_vilaca@outlook.com>
2025-07-21 20:31:00 -0300

    feat: add calculadora code

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+def soma(a, b):
+    """
+    Essa função recebe dois números e retorna a soma deles.
+    Parameters:
+    a (int or float): O primeiro número a ser adicionado.
+    b (int or float): O segundo número a ser adicionado.
+    Returns:
+    int or float: A soma dos dois números.
+    """
+    return a + b
+
+def subtracao(a, b):
+    """
+    Essa função recebe dois números e retorna a subtração do primeiro pelo segundo.
+    Parameters:
+    a (int or float): O número do qual será subtraído.
+    b (int or float): O número que será subtraído do primeiro.
+    Returns:
+    int or float: O resultado da subtração.
+    """
+    return a - b
